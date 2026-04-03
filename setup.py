1a93aafaaf7dbe11be7ec86323d9a06e5950966d
Chris Careaga <chris_careaga@sfu.ca>
2026-04-02 18:24:31 -0700

    pinning dependency version so things don't break

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setuptools.setup(
     version="0.0.1",
     author="Chris Careaga",
     author_email="chris_careaga@sfu.ca",
-    description='a package containing to the code for the paper "Intrinsic Image Decomposition via Ordinal Shading"',
+    description='a package containing the code for the paper "Intrinsic Image Decomposition via Ordinal Shading"',
     url="",
     packages=setuptools.find_packages(),
     license="",
     python_requires=">3.6",
     install_requires=[
-    'altered_midas @ git+https://github.com/CCareaga/MiDaS@master',
-    'chrislib @ git+https://github.com/CCareaga/chrislib@main'
+    'altered_midas @ git+https://github.com/CCareaga/MiDaS@fb51e3a',
+    'chrislib @ git+https://github.com/CCareaga/chrislib@9a4c63f'
     ]
 )
